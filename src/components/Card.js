dd1eff5be4e2c70f3d52781e5bd57783ba9ae813
Alex Harrison <alexjharrison@gmail.com>
2018-08-14 18:55:44 -0400

    Updates

--- src/components/Card.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Card = props => {
-    return <h2>{props.message}</h2>
-}
-
-export default Card;
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = props => (
-    <div className="navbar">
-
-    </div>
-)
-
-export default Header;
