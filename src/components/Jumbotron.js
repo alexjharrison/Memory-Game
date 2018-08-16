4a25b3ad56b151ae34b11ccfdb8818c4a7585306
Alex Harrison <alexjharrison@gmail.com>
2018-08-16 10:02:04 -0400

    fixed background image

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,8 +3,11 @@ import React from "react";
 
 
 const Jumbotron = () => {
+    const style = {
+        "background-image": "url('images/jumbotron.jpg')"
+    }
     return (
-        <div className="jumbotron center-align">
+        <div style={style} className="jumbotron center-align">
             <h1 className="display-4">Clicky App</h1>
             <h6>Don't click the same pupper twice</h6>
          </div>
